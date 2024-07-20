78c47c10bbd7b9b7202b0bbd3374e96ed338bcf6
Dion Timmer <diontimmer@live.nl>
2024-07-20 03:00:41 -0400

    add pip req

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="MetaVault",
     version="0.1",
     packages=[""],
+    download_url="https://github.com/diontimmer/MetaVault/archive/refs/tags/v_0.1.tar.gz",
     license="MIT",
     author="Dion Timmer",
     author_email="diontimmer@live.nl",
