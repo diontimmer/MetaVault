da6181460f4338fc58d9745d2d9566e2a356bc0b
Dion Timmer <diontimmer@live.nl>
2024-07-22 18:25:33 -0400

    add missing reqs

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     license="MIT",
     author="Dion Timmer",
     author_email="diontimmer@live.nl",
+    install_requires=["jsonlines", "pdoc"],
     description="A simple database for storing metadata associated with (media) files.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
