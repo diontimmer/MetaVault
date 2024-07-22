f2dc5383d1a9b3c210906799579a75740a903fc6
Dion Timmer <diontimmer@live.nl>
2024-07-22 16:52:33 -0400

    bump version

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
     name="MetaVault",
-    version="0.3",
-    download_url="https://github.com/diontimmer/MetaVault/archive/refs/tags/v_0.3.tar.gz",
+    version="0.4",
+    download_url="https://github.com/diontimmer/MetaVault/archive/refs/tags/v_0.4.tar.gz",
     license="MIT",
     author="Dion Timmer",
     author_email="diontimmer@live.nl",
